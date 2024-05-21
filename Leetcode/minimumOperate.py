4944dbe0cc6f0d353f2ce7eea63ead1e29d9a202
phantrunghieu91 <phantrunghieu1291@gmail.com>
2024-05-21 09:56:32 +0700

    minimum operation to make string of 1 and 0 alternating

---
+++ Leetcode/minimumOperate.py
@@ -0,0 +1,39 @@
+# You are given a string s consisting only of the characters '0' and '1'. In one operation, you can change any '0' to '1' or vice versa.
+# The string is called alternating if no two adjacent characters are equal. For example, the string "010" is alternating, while the string "0100" is not.
+# Return the minimum number of operations needed to make s alternating.
+# Example 1:
+# Input: s = "0100"
+# Output: 1
+# Explanation: If you change the last character to '1', s will be "0101", which is alternating.
+# Example 2:
+# Input: s = "10"
+# Output: 0
+# Explanation: s is already alternating.
+# Example 3:
+# Input: s = "1111"
+# Output: 2
+# Explanation: You need two operations to reach "0101" or "1010".
+# Constraints:
+#     1 <= s.length <= 104
+#     s[i] is either '0' or '1'.
+def minimum_operate(s):
+  if(len(s) == 1): return 0
+  count_1 = 0
+  count_2 = 0
+  s_1 = ''
+  s_2 = ''
+  for i in range(len(s)):
+    if(i % 2 == 0) :
+      s_1 += '0'
+      s_2 += '1'
+    else:
+      s_1 += '1'
+      s_2 += '0'
+    if(s[i] != s_1[i]):
+      count_1 += 1
+    if(s[i] != s_2[i]):
+      count_2 += 1
+  return min(count_1, count_2)
+
+# Test cases
+print(minimum_operate("10010100")) # 3
